0d6a0686237744428693b2814fe1600097ca3473
Derek Robati <derek.robati@gmail.com>
2019-03-16 00:48:02 -0400

    Throw it in a suite.

--- scripts/louds.test.js
+++ scripts/louds.test.js
@@ -1,15 +1,17 @@
 import { listenForLouds } from './louds';
 
-test('should respond to loud', () => {
-    const mockChannelSend = jest.fn(x => x);
-    const message = { content: 'fear', channel: { send: mockChannelSend } };
-    listenForLouds(message);
-    expect(mockChannelSend.mock.calls).toHaveLength(1);
-});
+describe('louds', () => {
+    test('should respond to loud', () => {
+        const mockChannelSend = jest.fn(x => x);
+        const message = { content: 'fear', channel: { send: mockChannelSend } };
+        listenForLouds(message);
+        expect(mockChannelSend.mock.calls).toHaveLength(1);
+    });
 
-test('should not respond to regular message', () => {
-    const mockChannelSend = jest.fn(x => x);
-    const message = { content: 'message', channel: { send: mockChannelSend } };
-    listenForLouds(message);
-    expect(mockChannelSend.mock.calls).toHaveLength(0);
+    test('should not respond to regular message', () => {
+        const mockChannelSend = jest.fn(x => x);
+        const message = { content: 'message', channel: { send: mockChannelSend } };
+        listenForLouds(message);
+        expect(mockChannelSend.mock.calls).toHaveLength(0);
+    });
 });
