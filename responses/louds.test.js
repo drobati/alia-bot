7ab3fcb9e28eb4ff3d889fb23e94b70cc68464fd
Derek Robati <derek.robati@gmail.com>
2019-03-17 00:51:28 -0400

    tests!

--- responses/louds.test.js
+++ responses/louds.test.js
@@ -1,18 +1,49 @@
-const { listenForLouds } = require('./louds');
+const louds = require('./louds');
 
-// TODO: Mock sql.
 describe('response/louds', () => {
-    test('should respond to loud', () => {
-        const mockChannelSend = jest.fn(x => x);
-        const message = { content: 'FEAR', channel: { send: mockChannelSend } };
-        listenForLouds(message);
-        expect(mockChannelSend.mock.calls).toHaveLength(1);
+    describe('should respond to loud', () => {
+        let message, model, mockChannelSend;
+
+        beforeEach(() => {
+            mockChannelSend = jest.fn();
+            const oldLoud = {
+                increment: jest.fn(),
+                message: 'MIND',
+            };
+            const newLoud = {
+                increment: jest.fn(),
+                message: 'KILLER',
+            };
+            message = { content: 'FEAR', channel: { send: mockChannelSend } };
+            model = {
+                Louds: {
+                    create: jest.fn().mockName('createLouds'),
+                    findOne: jest
+                        .fn()
+                        .mockResolvedValueOnce(oldLoud)
+                        .mockResolvedValueOnce(newLoud)
+                        .mockName('findOneLouds'),
+                },
+                Louds_Banned: {
+                    findOne: jest
+                        .fn()
+                        .mockResolvedValue(false)
+                        .mockName('Louds_Banned'),
+                },
+            };
+        });
+
+        test('one time', async () => {
+            await louds(message, model);
+            const sent = message.channel.send.mock.calls;
+            expect(sent).toHaveLength(1);
+        });
     });
 
-    test('should not respond to regular message', () => {
+    test('should not respond to regular message', async () => {
         const mockChannelSend = jest.fn(x => x);
         const message = { content: 'fear', channel: { send: mockChannelSend } };
-        listenForLouds(message);
+        await louds(message);
         expect(mockChannelSend.mock.calls).toHaveLength(0);
     });
 });
