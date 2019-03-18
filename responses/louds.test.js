8ae63cd1b118ca90b5ddcb63a829e2bb46a88e92
Derek Robati <derek.robati@gmail.com>
2019-03-17 22:14:43 -0400

    Added 5 new tests!

--- responses/louds.test.js
+++ responses/louds.test.js
@@ -1,27 +1,32 @@
 const louds = require('./louds');
 
 describe('response/louds', () => {
-    describe('should respond to loud', () => {
+    describe('should', () => {
         let message, model, mockChannelSend;
+        let oldLoud, newLoud;
 
         beforeEach(() => {
             mockChannelSend = jest.fn();
-            const oldLoud = {
+            oldLoud = {
                 increment: jest.fn(),
                 message: 'MIND',
             };
-            const newLoud = {
+            newLoud = {
                 increment: jest.fn(),
                 message: 'KILLER',
             };
-            message = { content: 'FEAR', channel: { send: mockChannelSend } };
+            message = {
+                content: 'FEAR',
+                author: { username: 'derek' },
+                channel: { send: mockChannelSend },
+            };
             model = {
                 Louds: {
                     create: jest.fn().mockName('createLouds'),
                     findOne: jest
                         .fn()
                         .mockResolvedValueOnce(oldLoud)
-                        .mockResolvedValueOnce(newLoud)
+                        .mockResolvedValueOnce(false)
                         .mockName('findOneLouds'),
                 },
                 Louds_Banned: {
@@ -33,17 +38,62 @@ describe('response/louds', () => {
             };
         });
 
-        test('one time', async () => {
+        test('respond to loud one time', async () => {
             await louds(message, model);
             const sent = message.channel.send.mock.calls;
             expect(sent).toHaveLength(1);
         });
-    });
 
-    // test('should not respond to regular message', async () => {
-    //     const mockChannelSend = jest.fn(x => x);
-    //     const message = { content: 'fear', channel: { send: mockChannelSend } };
-    //     await louds(message);
-    //     expect(mockChannelSend.mock.calls).toHaveLength(0);
-    // });
+        test('not respond if not a loud', async () => {
+            message.content = 'fear';
+            await louds(message, model);
+            const sent = message.channel.send.mock.calls;
+            expect(sent).toHaveLength(0);
+        });
+
+        test('respond to no louds in db', async () => {
+            model.Louds.findOne = jest
+                .fn()
+                .mockResolvedValueOnce(false)
+                .mockResolvedValueOnce(false)
+                .mockName('emptyLouds');
+            await louds(message, model);
+            const sent = message.channel.send.mock.calls[0][0];
+            expect(sent).toMatch(/No louds stored yet\./);
+        });
+
+        test('increment oldLoud usage count', async () => {
+            await louds(message, model);
+            const incremented = oldLoud.increment.mock.calls;
+            expect(incremented).toHaveLength(1);
+        });
+
+        test('check for loud already stored', async () => {
+            model.Louds.findOne = jest
+                .fn()
+                .mockResolvedValueOnce(false)
+                .mockResolvedValueOnce(true)
+                .mockName('existsLouds');
+            await louds(message, model);
+            const findOne = await model.Louds.findOne.mock.results[1].value;
+            expect(findOne).toBeTruthy();
+        });
+
+        test('check for banned loud', async () => {
+            model.Louds_Banned.findOne = jest
+                .fn()
+                .mockResolvedValue(true)
+                .mockName('existsLouds_Banned');
+            await louds(message, model);
+            const findOne = await model.Louds_Banned.findOne.mock.results[0].value;
+            expect(findOne).toBeTruthy();
+        });
+
+        test('store newLoud', async () => {
+            await louds(message, model);
+            const create = model.Louds.create;
+            const stored = { message: message.content, username: message.author.username };
+            expect(create).toHaveBeenCalledWith(stored);
+        });
+    });
 });
