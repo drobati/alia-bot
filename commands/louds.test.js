ae264cf8f271193f72a8a87bc0fbe7b976dea99f
Derek Robati <derek.robati@gmail.com>
2019-03-20 01:30:21 -0400

    Adding tests.

---
+++ commands/louds.test.js
@@ -0,0 +1,30 @@
+const louds = require('./louds');
+
+describe('commands/louds', () => {
+    describe('should', () => {
+        test('respond to delete', () => {
+            expect(false).toBeTruthy();
+        });
+        test('respond to ban', () => {
+            expect(false).toBeTruthy();
+        });
+        test('respond to unban', () => {
+            expect(false).toBeTruthy();
+        });
+        test('delete loud with delete command', () => {
+            expect(false).toBeTruthy();
+        });
+        test('add banned loud with ban command', () => {
+            expect(false).toBeTruthy();
+        });
+        test('delete loud with ban command', () => {
+            expect(false).toBeTruthy();
+        });
+        test('add loud with unban command', () => {
+            expect(false).toBeTruthy();
+        });
+        test('delete banned loud with unban command', () => {
+            expect(false).toBeTruthy();
+        });
+    });
+});
