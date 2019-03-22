ec7e5375381cf6fe3a7033cfbe8cf96ce2f46cad
Derek Robati <derek.robati@gmail.com>
2019-03-21 23:57:14 -0400

    Added twitch model.

---
+++ models/twitch.js
@@ -0,0 +1,14 @@
+const { Sequelize } = require('sequelize');
+
+module.exports = sequelize => {
+    const Twitch = sequelize.define('twitch', {
+        user_id: {
+            type: Sequelize.INTEGER,
+            unique: true,
+        },
+        twtich_id: {
+            type: Sequelize.STRING,
+            unique: true,
+        },
+    });
+};
