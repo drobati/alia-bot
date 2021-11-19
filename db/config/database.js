7cdf88bc37271dbdb88e1ca5593b7bd11b68992c
Derek Robati <derek.robati@gmail.com>
2021-11-19 16:58:23 -0500

    Migrated database to new type.

---
+++ db/config/database.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+module.exports = {
+    prod: {
+        username: 'root',
+        password: process.env.MYSQLDB_ROOT_PASSWORD,
+        database: process.env.MYSQLDB_DATABASE,
+        host: 'localhost',
+        port: 3307,
+        dialect: 'mysql'
+    }
+};
