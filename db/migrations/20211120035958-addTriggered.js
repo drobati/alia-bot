7c7efd458dffecdb4800557f6d27210ff3d6ec61
Derek Robati <derek@robati.com>
2023-11-17 16:57:30 -0500

    Ran some lint changes on this code.

--- db/migrations/20211120035958-addTriggered.js
+++ db/migrations/20211120035958-addTriggered.js
@@ -3,26 +3,26 @@
 module.exports = {
     up: async (queryInterface, Sequelize) => {
         await queryInterface.sequelize.transaction(
-            async (transaction) =>
+            async transaction =>
                 await queryInterface.addColumn(
                     'memories',
                     'triggered',
                     {
                         type: Sequelize.BOOLEAN,
                         defaultValue: false,
-                        allowNull: false
+                        allowNull: false,
                     },
                     {
-                        transaction
-                    }
-                )
+                        transaction,
+                    },
+                ),
         );
     },
 
-    down: async (queryInterface) => {
+    down: async queryInterface => {
         await queryInterface.sequelize.transaction(
-            async (transaction) =>
-                await queryInterface.removeColumn('memories', 'triggered', { transaction })
+            async transaction =>
+                await queryInterface.removeColumn('memories', 'triggered', { transaction }),
         );
-    }
+    },
 };
